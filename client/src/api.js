212656a0e251a67fa01c4eff10b620e6a5d12a96
Karan7503 <karan752003@gmail.com>
2025-08-05 19:06:21 +0530

    made changes to axios to take render frontend url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: "http://localhost:5000",
+//   withCredentials: true,
+// });
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
